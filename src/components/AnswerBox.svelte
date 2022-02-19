df5aa44799c16cc196f1f67b2860701256e6cc46
Kukuh Sulistyo <sulistyo.kukuh@gmail.com>
2022-02-19 20:30:01 +0700

    feat(AnswerBox): add reveal animationi

--- src/components/AnswerBox.svelte
+++ src/components/AnswerBox.svelte
@@ -1,5 +1,5 @@
 <script context="module" lang="ts">
-	export type AnswerBoxVariant = 'correct' | 'focused' | 'idle' | 'misplaced' | 'wrong';
+	export type AnswerBoxStatus = 'idle' | 'correct' | 'misplaced' | 'wrong';
 </script>
 
 <script lang="ts">
@@ -9,18 +9,18 @@
 	import CheckIcon from '../icons/CheckIcon.svelte';
 	import XmarkIcon from '../icons/XmarkIcon.svelte';
 
+	export let isFocused: boolean = false;
+	export let isRevealed: boolean = false;
+	export let order: number = 1;
+	export let status: AnswerBoxStatus = 'idle';
 	export let value: string = '';
-	export let variant: AnswerBoxVariant = 'idle';
 
 	let backgroundStyle = 'bg-stone-900';
 	let borderStyle = '';
 
-	$: switch (variant) {
+	$: switch (status) {
 		case 'correct':
 			backgroundStyle = 'bg-emerald-800';
-			break;
-		case 'focused':
-			borderStyle = 'border border-dashed border-neutral-300';
 			break;
 		case 'idle':
 			borderStyle = 'border border-neutral-700';
@@ -35,31 +35,68 @@
 </script>
 
 <div
-	class={clsx(
-		'| root',
-		'| relative | flex justify-center items-center',
-		'| w-full h-full',
-		`| ${backgroundStyle} ${borderStyle} uppercase text-neutral-100 text-2xl xxs:text-3xl xs:text-4xl uppercase`
-	)}
+	class={clsx('| root', '| relative w-full h-full')}
+	class:revealed={isRevealed}
+	style={`--animation-order: ${order}`}
 >
-	<span
-		class="absolute bottom-0 left-0 | w-3 xxs:w-4 xs:w-5 h-3 xxs:h-4 xs:h-5 text-neutral-400"
+	<!-- Front -->
+	<div
+		class={clsx(
+			'| box front',
+			'| w-full h-full',
+			'| flex justify-center items-center',
+			`| bg-stone-900 border uppercase text-neutral-100 text-2xl xxs:text-3xl xs:text-4xl uppercase`,
+			isFocused ? 'border-dashed border-neutral-300' : 'border-neutral-700'
+		)}
 	>
-		{#if variant === 'correct'}
-			<CheckIcon />
-		{:else if variant === 'misplaced'}
-			<CircleIcon />
-		{:else if variant === 'wrong'}
-			<XmarkIcon />
-		{/if}
-	</span>
-	{value}
+		{value}
+	</div>
+
+	<!-- Back -->
+	<div
+		class={clsx(
+			'| box box-back',
+			'| w-full h-full',
+			'| flex justify-center items-center',
+			`| ${backgroundStyle} ${borderStyle} uppercase text-neutral-100 text-2xl xxs:text-3xl xs:text-4xl uppercase`
+		)}
+	>
+		<span class="absolute bottom-0 left-0 | w-3 xxs:w-4 xs:w-5 h-3 xxs:h-4 xs:h-5 text-neutral-400">
+			{#if status === 'correct'}
+				<CheckIcon />
+			{:else if status === 'misplaced'}
+				<CircleIcon />
+			{:else if status === 'wrong'}
+				<XmarkIcon />
+			{/if}
+		</span>
+		{value}
+	</div>
 </div>
 
 <style>
-	.root::before {
+	.root {
+		transform-style: preserve-3d;
+		transition: transform 0.8s;
+		transition-delay: calc(var(--animation-order) * 200ms);
+	}
+	.root.revealed {
+		transform: rotateY(180deg);
+	}
+
+	.box::before {
 		content: '';
 		padding-bottom: 100%;
 		display: block;
+	}
+	.box.box-front,
+	.box.box-back {
+		position: absolute;
+		top: 0;
+		-webkit-backface-visibility: hidden;
+		backface-visibility: hidden;
+	}
+	.box.box-back {
+		transform: rotateY(180deg);
 	}
 </style>
