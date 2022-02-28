b897e27de94d3d350f42a4cf4453e94696989351
Kukuh Sulistyo <sulistyo.kukuh@gmail.com>
2022-02-28 20:00:15 +0700

    feat: show modal when success and failed to answer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
 	import { onDestroy, onMount } from 'svelte';
 
+	import CountdownTimer from '../components/CountdownTimer.svelte';
 	import GuessBox from '../components/GuessBox.svelte';
 	import KeypadButton from '../components/KeypadButton.svelte';
+	import Modal from '../components/Modal.svelte';
+	import Note from '../components/Note.svelte';
+	import { ToastContainer } from '../components/Toast';
 	import DeleteIcon from '../icons/DeleteIcon.svelte';
 	import ReturnIcon from '../icons/ReturnIcon.svelte';
-	import { ToastContainer } from '../components/Toast';
 
 	import {
 		checkFocus,
 		decode,
+		getSecondsToTomorrow,
 		KeypadLetter,
 		KeypadLetterStatus,
 		MAX_ATTEMPT,
 		VALID_LETTERS,
 		WORD_LENGTH
 	} from '$lib';
-	import { animateWrongGuessStore, guessAttemptsStore } from '../stores';
+	import { animateWrongGuessStore, gameStatusStore, guessAttemptsStore } from '../stores';
 
 	// Props
 	//
@@ -26,7 +30,14 @@
 	// States
 	//
 	let animateWrongGuessTimer = null;
-	$: answer = decode(encryptedAnswer);
+	let modalType: 'no-modal' | 'statistic-modal' | 'info-modal' = 'no-modal';
+	let { date, word: answer, wordDetail } = decode(encryptedAnswer);
+
+	$: {
+		if ($gameStatusStore === 'answered-correct' || $gameStatusStore === 'answered-wrong') {
+			modalType = 'statistic-modal';
+		}
+	}
 
 	const unsubscribeAnimateWrongGuessStore = animateWrongGuessStore.subscribe(
 		(animateWrongGuess) => {
@@ -71,26 +82,54 @@
 		}));
 	};
 
+	let revealCurrentGuess = () => {
+		if (
+			modalType === 'no-modal' &&
+			$gameStatusStore !== 'answered-correct' &&
+			$gameStatusStore !== 'answered-wrong'
+		) {
+			guessAttemptsStore.revealCurrentGuess(answer, wordpool);
+		}
+	};
+
+	let appendLetterToCurrentGuess = (letter: string) => {
+		if (
+			modalType === 'no-modal' &&
+			$gameStatusStore !== 'answered-correct' &&
+			$gameStatusStore !== 'answered-wrong'
+		) {
+			guessAttemptsStore.appendLetterToCurrentGuess(letter.toLowerCase());
+		}
+	};
+
+	let removeLastLetterFromCurrentGuess = () => {
+		if (
+			modalType === 'no-modal' &&
+			$gameStatusStore !== 'answered-correct' &&
+			$gameStatusStore !== 'answered-wrong'
+		) {
+			guessAttemptsStore.removeLastLetterFromCurrentGuess();
+		}
+	};
+
 	onMount(() => {
 		guessAttemptsStore.revealAllGuess(answer);
 		revealKeypadLetterStatus();
 
 		document.addEventListener('keydown', (e) => {
 			if (e.code === 'Enter') {
-				guessAttemptsStore.revealCurrentGuess(answer, wordpool);
+				revealCurrentGuess();
 				revealKeypadLetterStatus();
 			} else if (e.code === 'Backspace') {
-				// TODO: pop letter from the current guess
-				guessAttemptsStore.removeLastLetterFromCurrentGuess();
+				removeLastLetterFromCurrentGuess();
 			} else {
-				guessAttemptsStore.appendLetterToCurrentGuess(e.key.toLowerCase());
+				appendLetterToCurrentGuess(e.key);
 			}
 		});
 	});
 </script>
 
 <div class="w-screen min-h-screen | bg-surface | font-sans">
-	<header />
 	<main class="max-w-lg mx-auto | px-8">
 		<!-- Answer Boxes -->
 		<div class="answer-container | grid gap-2 grid-cols-5 grid-rows-6 | mb-8">
@@ -129,10 +168,10 @@
 					<KeypadButton
 						style="shrink-0"
 						status={keypadLetter.status}
-						on:click={() =>
-							guessAttemptsStore.appendLetterToCurrentGuess(keypadLetter.letter.toLowerCase())}
-						>{keypadLetter.letter}</KeypadButton
+						on:click={() => appendLetterToCurrentGuess(keypadLetter.letter)}
 					>
+						{keypadLetter.letter}
+					</KeypadButton>
 				{/each}
 			</div>
 			<div class="flex justify-center | space-x-1 xs:space-x-2">
@@ -140,17 +179,17 @@
 					<KeypadButton
 						style="shrink-0"
 						status={keypadLetter.status}
-						on:click={() =>
-							guessAttemptsStore.appendLetterToCurrentGuess(keypadLetter.letter.toLowerCase())}
-						>{keypadLetter.letter}</KeypadButton
+						on:click={() => appendLetterToCurrentGuess(keypadLetter.letter)}
 					>
+						{keypadLetter.letter}
+					</KeypadButton>
 				{/each}
 			</div>
 			<div class="flex justify-center | space-x-1 xs:space-x-2">
 				<KeypadButton
 					isCustomWidth
 					style="flex items-center justify-center shrink-0 | w-10 xxs:w-11 xs:w-14"
-					on:click={() => guessAttemptsStore.removeLastLetterFromCurrentGuess()}
+					on:click={removeLastLetterFromCurrentGuess}
 				>
 					<span class="w-em"><DeleteIcon /></span>
 					<span class="sr-only">Hapus</span>
@@ -159,15 +198,18 @@
 					<KeypadButton
 						style="shrink-0"
 						status={keypadLetter.status}
-						on:click={() =>
-							guessAttemptsStore.appendLetterToCurrentGuess(keypadLetter.letter.toLowerCase())}
-						>{keypadLetter.letter}</KeypadButton
+						on:click={() => appendLetterToCurrentGuess(keypadLetter.letter)}
 					>
+						{keypadLetter.letter}
+					</KeypadButton>
 				{/each}
 				<KeypadButton
 					isCustomWidth
 					style="flex items-center justify-center shrink-0 | w-10 xxs:w-11 xs:w-14"
-					on:click={() => guessAttemptsStore.revealCurrentGuess(answer, wordpool)}
+					on:click={() => {
+						revealKeypadLetterStatus();
+						revealCurrentGuess();
+					}}
 				>
 					<span class="w-em"><ReturnIcon /></span>
 					<span class="sr-only">Ok</span>
@@ -175,6 +217,37 @@
 			</div>
 		</div>
 	</main>
+
+	<Modal
+		isOpen={modalType === 'statistic-modal'}
+		title="Statistik"
+		onClose={() => {
+			modalType = 'no-modal';
+		}}
+	>
+		<div class="px-3 py-4 space-y-4 | border-b border-neutral-700">
+			{#if $gameStatusStore === 'answered-correct'}
+				<Note variant="success">Mantap! Bedhekanmu bener</Note>
+				<div class="mt-4">
+					<p class="font-bold text-2xl">{answer}</p>
+					{#if wordDetail.phoneticTranscription || wordDetail.types?.length > 0}
+						<p class="text-sm text-neutral-400 | mb-2">
+							{`/${wordDetail.phoneticTranscription}/ • ${wordDetail.types.join(', ')}`}
+						</p>
+					{/if}
+					<p class="text-base text-neutral-200">{wordDetail.indonesianTranslation}</p>
+				</div>
+			{/if}
+
+			{#if $gameStatusStore === 'answered-wrong'}
+				<Note variant="danger">Jancuk! Bedhekanmu salah!!!</Note>
+			{/if}
+		</div>
+		<div class="px-3 py-4">
+			<p class="text-neutral-300">Bedhekan selanjute bakal dibagikaken ing:</p>
+			<CountdownTimer class="text-2xl" duration={getSecondsToTomorrow(date)} />
+		</div>
+	</Modal>
 </div>
 
 <ToastContainer />
