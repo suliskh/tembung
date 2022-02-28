6df8ef92b68232a8d1e03a95900d0b471861b8d5
Kukuh Sulistyo <sulistyo.kukuh@gmail.com>
2022-02-28 21:17:25 +0700

    feat: add shadow-button-wrong box shadow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
 		extend: {
 			boxShadow: {
 				button: '0px 3px 0px #292524',
+				'button-wrong': '0px 3px 0px #1c1917',
 				'button-active': 'inset 0px 0px 4px 2px rgba(44, 44, 44, 0.5)'
 			},
 			colors: {
