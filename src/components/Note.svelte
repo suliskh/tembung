db0628f12fa0c2b6398c4f13af1e5a0e71e83824
Kukuh Sulistyo <sulistyo.kukuh@gmail.com>
2022-02-28 13:43:08 +0700

    feat: add Note component

---
+++ src/components/Note.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import clsx from 'clsx';
+
+	// Props
+	//
+	export let variant: 'success' | 'danger';
+</script>
+
+<div
+	class={clsx(
+		'flex | p-3',
+		'| rounded-md',
+		'| text-neutral-300',
+		variant === 'success' ? 'border-l-4 border-emerald-800 bg-emerald-900/20' : '',
+		variant === 'danger' ? 'border-l-4 border-rose-800 bg-rose-900/20' : ''
+	)}
+>
+	<slot />
+</div>
