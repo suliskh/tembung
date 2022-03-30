c0dd1b4585c2c6f135a2714f4a389392582e0687
Kukuh Sulistyo <sulistyo.kukuh@gmail.com>
2022-03-30 10:48:52 +0700

    feat: Add tebakan page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,257 +1,248 @@
+<script context="module" lang="ts">
+	export function checkGuessBoxFocus(
+		letterIndex: number,
+		answerLength: number,
+		word: string,
+		isRevealed: boolean
+	): boolean {
+		let isLastIndex = letterIndex === answerLength - 1;
+		let isFull = word.length >= answerLength;
+		let isToBeFill = letterIndex === word.length;
+
+		return isToBeFill || (isLastIndex && !isRevealed && isFull);
+	}
+
+	export function checkGuessBoxStatus(
+		letterIndex: number,
+		letter: string,
+		answer: string,
+		isRevealed: boolean
+	): GuessLetterStatus {
+		let status: GuessLetterStatus = 'idle';
+
+		if (isRevealed) {
+			let normalizedAnswer = answer.toUpperCase();
+			let isLetterInAnswer = normalizedAnswer.includes(letter);
+			console.log(letter, normalizedAnswer, isLetterInAnswer);
+
+			if (isLetterInAnswer) {
+				let isSamePos = letter === normalizedAnswer[letterIndex];
+				status = isSamePos ? 'correct' : 'misplaced';
+			} else {
+				status = 'wrong';
+			}
+		}
+
+		return status;
+	}
+</script>
+
 <script lang="ts">
-	import { onDestroy, onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
-	import CountdownTimer from '../components/CountdownTimer.svelte';
 	import GuessBox from '../components/GuessBox.svelte';
-	import KeypadButton from '../components/KeypadButton.svelte';
 	import Modal from '../components/Modal.svelte';
-	import Note from '../components/Note.svelte';
 	import { ToastContainer } from '../components/Toast';
-	import DeleteIcon from '../icons/DeleteIcon.svelte';
-	import ReturnIcon from '../icons/ReturnIcon.svelte';
 
-	import {
-		checkFocus,
-		decode,
-		getSecondsToTomorrow,
-		GuessLetterStatus,
-		KeypadLetter,
-		MAX_ATTEMPT,
-		STORAGE_KEY_GUESS_ATTEMPTS,
-		VALID_LETTERS,
-		WORD_LENGTH
-	} from '$lib';
-	import { animateWrongGuessStore, gameStatusStore, guessAttemptsStore } from '../stores';
-
-	// Props
-	//
-	export let encryptedAnswer: string = '';
-	export let wordpool: Array<string> = [];
+	import { GuessLetterStatus, MAX_ATTEMPT } from '$lib';
 
 	// States
 	//
-	let animateWrongGuessTimer = null;
-	let modalType: 'no-modal' | 'statistic-modal' | 'info-modal' = 'no-modal';
-	let { date, word: answer, wordDetail } = decode(encryptedAnswer);
-
-	$: {
-		if ($gameStatusStore === 'answered-correct' || $gameStatusStore === 'answered-wrong') {
-			modalType = 'statistic-modal';
+	let quizzes = [
+		{
+			question: 'Satu, dua, tiga, ...',
+			answer: 'empat',
+			img: 'https://cdn.pixabay.com/photo/2016/02/08/12/25/number-1186417_960_720.png'
+		},
+		{
+			question: 'Salah satu anggota di Tim Ruangkelas?',
+			answer: 'guntur',
+			img: 'https://media-exp1.licdn.com/dms/image/C4E03AQHallzvAcPqxg/profile-displayphoto-shrink_800_800/0/1517057406296?e=1654128000&v=beta&t=PsTIMJLUS96TR2_RiLLjXceWQ-pPovrHqh8Tj7ou_RM'
+		},
+		{
+			question: 'Kaka dan bimbim adalah nama personel yang di...',
+			answer: 'ulang',
+			img: 'https://lingopolo.org/thai/sites/lingopolo.org.thai/files/styles/entry/public/images/2015/11/02/repeat.jpg'
+		},
+		{
+			question: 'Penjara akan sangat berbahaya jika para napinya...',
+			answer: 'batuk',
+			img: 'https://akcdn.detik.net.id/visual/2019/04/21/f40154d1-133a-4aec-a7c7-5cf2111f2f4d_169.jpeg?w=650'
+		},
+		{
+			question: 'Seorang pejabat akan terkena masalah apabila tersangkut...',
+			answer: 'jemuran',
+			img: 'https://img.okezone.com/content/2011/11/25/214/534010/KnUKHrMwcy.jpg'
+		},
+		{
+			question: '"B" adalah tanda untuk kendaraan yang berasal dari...',
+			answer: 'alfabet',
+			img: 'https://risetcdn.jatimtimes.com/images/2021/07/07/Alfabet-Foto-Infia.co-Pe4fb5dc60e437db5.md.jpg'
+		},
+		{
+			question: 'Bulan dan bintang bisa dilihat di...',
+			answer: 'Malang',
+			img: 'https://sites.google.com/site/dutchskies/_/rsrc/1231374345208/general/filecabinet/Moon_Venus_Saturn_20070618_2157_web.jpg?width=100%25'
+		},
+		{
+			question: 'Selain motor, truk, dan bis, di jalan raya juga ada... ',
+			answer: '-----',
+			img: 'https://t-3.tstatic.net/jualbeli/img/njajal/2019/6/Agar-Mudik-Bebas-Tilang--Kenali-Marka-Jalan-Beserta-Fungsinya-master-1945893826.jpg'
 		}
-	}
+	];
+	let guesses = [{ word: '', isRevealed: false }];
+	let isAnswerModalOpen = false;
+	let isWarningModalOpen = false;
 
-	const unsubscribeAnimateWrongGuessStore = animateWrongGuessStore.subscribe(
-		(animateWrongGuess) => {
-			if (animateWrongGuess) {
-				clearTimeout(animateWrongGuessTimer);
-				setTimeout(() => animateWrongGuessStore.update(() => false), 1000);
-			}
-		}
-	);
+	let currentQuizIndex = 0;
+	let currentAttempt = 0;
+	$: currentAnswer = quizzes[currentQuizIndex].answer;
 
-	onDestroy(unsubscribeAnimateWrongGuessStore);
+	let appendLetterToCurrentGuess = (letter) => {
+		let isCurrentAttemptFull = guesses[currentAttempt].word.length >= currentAnswer.length;
 
-	let keypadLetters: Array<KeypadLetter> = VALID_LETTERS.map((letter) => ({
-		letter,
-		status: 'idle'
-	}));
-
-	let revealKeypadLetterStatus = () => {
-		let revealedKeypadLettersStatus = new Map<string, GuessLetterStatus>();
-
-		$guessAttemptsStore.forEach((guessAttempt) => {
-			guessAttempt.word.split('').forEach((letter, i) => {
-				let isNormal = revealedKeypadLettersStatus.get(letter) === 'idle';
-				let isMisplaced = revealedKeypadLettersStatus.get(letter) === 'misplaced';
-				let isEmpty = !Boolean(revealedKeypadLettersStatus.get(letter));
-
-				if (isEmpty || isMisplaced || isNormal) {
-					if (guessAttempt.statuses[i] === 'correct') {
-						revealedKeypadLettersStatus.set(letter, 'correct');
-					} else if (guessAttempt.statuses[i] === 'wrong') {
-						revealedKeypadLettersStatus.set(letter, 'wrong');
-					} else if (guessAttempt.statuses[i] === 'misplaced') {
-						revealedKeypadLettersStatus.set(letter, 'misplaced');
-					}
-				}
-			});
-		});
-
-		keypadLetters = keypadLetters.map(({ letter, status }) => ({
-			letter,
-			status: revealedKeypadLettersStatus.get(letter) || status
-		}));
-	};
-
-	let revealCurrentGuess = () => {
-		if (
-			modalType === 'no-modal' &&
-			$gameStatusStore !== 'answered-correct' &&
-			$gameStatusStore !== 'answered-wrong'
-		) {
-			guessAttemptsStore.revealCurrentGuess(answer, wordpool);
-		}
-	};
-
-	let appendLetterToCurrentGuess = (letter: string) => {
-		if (
-			modalType === 'no-modal' &&
-			$gameStatusStore !== 'answered-correct' &&
-			$gameStatusStore !== 'answered-wrong'
-		) {
-			guessAttemptsStore.appendLetterToCurrentGuess(letter.toLowerCase());
+		if (!isCurrentAttemptFull) {
+			guesses[currentAttempt].word = guesses[currentAttempt].word.concat(letter);
 		}
 	};
 
 	let removeLastLetterFromCurrentGuess = () => {
-		if (
-			modalType === 'no-modal' &&
-			$gameStatusStore !== 'answered-correct' &&
-			$gameStatusStore !== 'answered-wrong'
-		) {
-			guessAttemptsStore.removeLastLetterFromCurrentGuess();
+		let currentGuessWord = guesses[currentAttempt].word;
+		guesses[currentAttempt].word = currentGuessWord.substring(0, currentGuessWord.length - 1);
+	};
+
+	let revealCurrentGuess = () => {
+		let isCurrentAttemptFull = guesses[currentAttempt].word.length >= currentAnswer.length;
+
+		if (isCurrentAttemptFull) {
+			guesses[currentAttempt].isRevealed = true;
+			if (currentAttempt < MAX_ATTEMPT - 1) {
+				currentAttempt = currentAttempt + 1;
+				guesses.push({ word: '', isRevealed: false });
+			}
 		}
 	};
 
+	$: console.log(isAnswerModalOpen);
+
 	onMount(() => {
-		let storedGuessWords = localStorage.getItem(STORAGE_KEY_GUESS_ATTEMPTS);
-		if (storedGuessWords) {
-			guessAttemptsStore.addAllWords(JSON.parse(storedGuessWords));
-		}
-
-		guessAttemptsStore.revealAllGuess(answer);
-		revealKeypadLetterStatus();
-
+		isWarningModalOpen = true;
 		document.addEventListener('keydown', (e) => {
 			if (e.code === 'Enter') {
 				revealCurrentGuess();
-				revealKeypadLetterStatus();
 			} else if (e.code === 'Backspace') {
 				removeLastLetterFromCurrentGuess();
 			} else {
-				appendLetterToCurrentGuess(e.key);
+				appendLetterToCurrentGuess(e.key.toUpperCase());
 			}
 		});
 	});
 </script>
 
 <div class="w-screen min-h-screen | bg-surface | font-sans">
-	<main class="max-w-lg mx-auto | px-8">
+	<main class="max-w-xl mx-auto | px-8">
+		<div class="py-10">
+			<p class="text-center text-neutral-500">{`${currentQuizIndex + 1}/${quizzes.length}`}</p>
+			<p class="text-4xl italic text-center text-neutral-200">
+				{quizzes[currentQuizIndex].question}
+			</p>
+		</div>
+		<div class="flex justify-between mb-3">
+			<button
+				class="shrink-0 | py-2 px-3 rounded xs:rounded-lg bg-stone-600 shadow-button active:shadow-button-active text-sm xxs:text-lg text-neutral-100 active:text-neutral-300 uppercase"
+				on:click={() => {
+					isAnswerModalOpen = true;
+				}}
+			>
+				Ungkap
+			</button>
+
+			<button
+				class="shrink-0 | py-2 px-3 rounded xs:rounded-lg bg-stone-600 shadow-button active:shadow-button-active text-sm xxs:text-lg text-neutral-100 active:text-neutral-300 uppercase"
+				on:click={() => {
+					if (currentQuizIndex < quizzes.length - 1) {
+						currentQuizIndex++;
+					}
+				}}
+			>
+				Selanjutnya
+			</button>
+		</div>
+
 		<!-- Answer Boxes -->
-		<div class="answer-container | grid gap-2 grid-cols-5 grid-rows-6 | mb-8">
+		<div class="flex flex-col space-y-2">
 			<!-- Guess Attempts -->
-			{#each $guessAttemptsStore as attempt, guessIndex (guessIndex)}
-				<div class="contents">
-					{#each Array(WORD_LENGTH).fill(1) as _x, letterIndex (letterIndex)}
+			{#each guesses as guess, guessIndex (guessIndex)}
+				<div class="flex space-x-2">
+					{#each Array(currentAnswer.length).fill(1) as _x, letterIndex (letterIndex)}
 						<GuessBox
-							isShaked={guessIndex === $guessAttemptsStore.length - 1 && $animateWrongGuessStore}
-							isFocused={checkFocus(letterIndex, attempt.word, attempt.isRevealed)}
-							isRevealed={attempt.isRevealed}
+							isFocused={checkGuessBoxFocus(
+								letterIndex,
+								currentAnswer.length,
+								guess.word,
+								guess.isRevealed
+							)}
+							isRevealed={guess.isRevealed}
 							order={letterIndex}
-							status={attempt.statuses[letterIndex]}
-							value={attempt.word[letterIndex] || ''}
+							status={checkGuessBoxStatus(
+								letterIndex,
+								guess.word[letterIndex],
+								currentAnswer,
+								guess.isRevealed
+							)}
+							value={guess.word[letterIndex] || ''}
 						/>
 					{/each}
 				</div>
 			{/each}
 
 			<!-- Remaining -->
-			{#if $guessAttemptsStore.length < MAX_ATTEMPT}
-				{#each Array(MAX_ATTEMPT - $guessAttemptsStore.length).fill(1) as i}
-					<div class="contents">
-						{#each Array(WORD_LENGTH).fill(1) as _x}
+			{#if guesses.length < MAX_ATTEMPT}
+				{#each Array(MAX_ATTEMPT - guesses.length).fill(1) as i}
+					<div class="flex space-x-2">
+						{#each Array(currentAnswer.length).fill(1) as _x}
 							<GuessBox isFocused={false} status="idle" value="" />
 						{/each}
 					</div>
 				{/each}
 			{/if}
 		</div>
-
-		<!-- Keypad -->
-		<div class="space-y-2 xs:space-y-3">
-			<div class="flex justify-center | space-x-1 xs:space-x-2">
-				{#each keypadLetters.slice(0, 10) as keypadLetter, i (i)}
-					<KeypadButton
-						style="shrink-0"
-						status={keypadLetter.status}
-						on:click={() => appendLetterToCurrentGuess(keypadLetter.letter)}
-					>
-						{keypadLetter.letter}
-					</KeypadButton>
-				{/each}
-			</div>
-			<div class="flex justify-center | space-x-1 xs:space-x-2">
-				{#each keypadLetters.slice(10, 19) as keypadLetter, i (i)}
-					<KeypadButton
-						style="shrink-0"
-						status={keypadLetter.status}
-						on:click={() => appendLetterToCurrentGuess(keypadLetter.letter)}
-					>
-						{keypadLetter.letter}
-					</KeypadButton>
-				{/each}
-			</div>
-			<div class="flex justify-center | space-x-1 xs:space-x-2">
-				<KeypadButton
-					isCustomWidth
-					style="flex items-center justify-center shrink-0 | w-10 xxs:w-11 xs:w-14"
-					on:click={removeLastLetterFromCurrentGuess}
-				>
-					<span class="w-em"><DeleteIcon /></span>
-					<span class="sr-only">Hapus</span>
-				</KeypadButton>
-				{#each keypadLetters.slice(19, 26) as keypadLetter, i (i)}
-					<KeypadButton
-						style="shrink-0"
-						status={keypadLetter.status}
-						on:click={() => appendLetterToCurrentGuess(keypadLetter.letter)}
-					>
-						{keypadLetter.letter}
-					</KeypadButton>
-				{/each}
-				<KeypadButton
-					isCustomWidth
-					style="flex items-center justify-center shrink-0 | w-10 xxs:w-11 xs:w-14"
-					on:click={() => {
-						revealCurrentGuess();
-						revealKeypadLetterStatus();
-					}}
-				>
-					<span class="w-em"><ReturnIcon /></span>
-					<span class="sr-only">Ok</span>
-				</KeypadButton>
-			</div>
-		</div>
 	</main>
 
 	<Modal
-		isOpen={modalType === 'statistic-modal'}
-		title="Statistik"
+		isOpen={isAnswerModalOpen}
+		title="Ungkap Jawaban"
 		onClose={() => {
-			modalType = 'no-modal';
+			isAnswerModalOpen = false;
 		}}
 	>
-		<div class="px-3 py-4 space-y-4 | border-b border-neutral-700">
-			{#if $gameStatusStore === 'answered-correct'}
-				<Note variant="success">Mantap! Bedhekanmu bener</Note>
-				<div class="mt-4">
-					<p class="font-bold text-2xl">{answer}</p>
-					{#if wordDetail.phoneticTranscription || wordDetail.types?.length > 0}
-						<p class="text-sm text-neutral-400 | mb-2">
-							{`/${wordDetail.phoneticTranscription}/ • ${wordDetail.types.join(', ')}`}
-						</p>
-					{/if}
-					<p class="text-base text-neutral-200">{wordDetail.indonesianTranslation}</p>
-				</div>
-			{/if}
-
-			{#if $gameStatusStore === 'answered-wrong'}
-				<Note variant="danger">Jancuk! Bedhekanmu salah!!!</Note>
-			{/if}
+		<div class="px-3 py-4 | flex flex-col items-center">
+			<p class="text-xl italic text-center text-neutral-400 mb-8">
+				{quizzes[currentQuizIndex].question}
+			</p>
+			<img
+				alt={quizzes[currentQuizIndex].answer.toUpperCase()}
+				class="w-56 mb-4"
+				src={quizzes[currentQuizIndex].img}
+			/>
+			<p
+				class="text-xl text-center tracking-widest bg-emerald-900/20 rounded-md border-l-4 border-r-4 border-emerald-800 | py-2 px-5"
+			>
+				{quizzes[currentQuizIndex].answer.toUpperCase()}
+			</p>
 		</div>
-		<div class="px-3 py-4">
-			<p class="text-neutral-300">Bedhekan selanjute bakal dibagikaken ing:</p>
-			<CountdownTimer class="text-2xl" duration={getSecondsToTomorrow(date)} />
+	</Modal>
+	<Modal
+		isOpen={isWarningModalOpen}
+		title="[!] Peringatan"
+		onClose={() => {
+			isWarningModalOpen = false;
+		}}
+	>
+		<div class="px-3 py-4 | flex flex-col items-center">
+			<p class="text-xl text-center text-neutral-400">
+				Memainkan permainan ini dapat menyebabkan hipertensi, serangan jantung, dan gangguan emosi dan kejiwaan.
+			</p>
 		</div>
 	</Modal>
 </div>
